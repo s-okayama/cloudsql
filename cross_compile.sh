3cf3faaec1796f72634d194668c4dbf4c5ad6cbb
s-okayama <sohei.okayama@uzabase.com>
2023-03-21 23:34:54 +0900

    Add windows.

--- cross_compile.sh
+++ cross_compile.sh
@@ -1,5 +1,5 @@
 SOURCE=cloudsql
-VERSION=v1.2.0
+VERSION=v1.2.4
 DIR=release
 
 mkdir -p ${DIR}
@@ -8,9 +8,13 @@ GOOS=darwin  GOARCH=arm64 go build -o ./${DIR}/${SOURCE}_${VERSION}_darwin_arm64
 GOOS=linux   GOARCH=386   go build -o ./${DIR}/${SOURCE}_${VERSION}_linux_386 ${SOURCE}.go
 GOOS=linux   GOARCH=amd64 go build -o ./${DIR}/${SOURCE}_${VERSION}_linux_amd64 ${SOURCE}.go
 GOOS=linux   GOARCH=arm64 go build -o ./${DIR}/${SOURCE}_${VERSION}_linux_arm64 ${SOURCE}.go
+GOOS=windows   GOARCH=arm64 go build -o ./${DIR}/${SOURCE}_${VERSION}_windows_amd64 ${SOURCE}.go
+GOOS=windows   GOARCH=arm64 go build -o ./${DIR}/${SOURCE}_${VERSION}_windows_arm64 ${SOURCE}.go
 
 tar zcvf ./${DIR}/${SOURCE}_${VERSION}_darwin_amd64.tar.gz ./${DIR}/${SOURCE}_${VERSION}_darwin_amd64
 tar zcvf ./${DIR}/${SOURCE}_${VERSION}_darwin_arm64.tar.gz ./${DIR}/${SOURCE}_${VERSION}_darwin_arm64
 tar zcvf ./${DIR}/${SOURCE}_${VERSION}_linux_386.tar.gz ./${DIR}/${SOURCE}_${VERSION}_linux_386
 tar zcvf ./${DIR}/${SOURCE}_${VERSION}_linux_amd64.tar.gz ./${DIR}/${SOURCE}_${VERSION}_linux_amd64
 tar zcvf ./${DIR}/${SOURCE}_${VERSION}_linux_arm64.tar.gz ./${DIR}/${SOURCE}_${VERSION}_linux_arm64
+tar zcvf ./${DIR}/${SOURCE}_${VERSION}_windows_amd64.tar.gz ./${DIR}/${SOURCE}_${VERSION}_windows_amd64
+tar zcvf ./${DIR}/${SOURCE}_${VERSION}_windows_arm64.tar.gz ./${DIR}/${SOURCE}_${VERSION}_windows_arm64
